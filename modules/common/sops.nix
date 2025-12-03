3e6bf9b25b1ac4d74a706f8db17305862539cff2
iivusly <52052910+iivusly@users.noreply.github.com>
2025-12-02 16:15:05 -0800

    fix: add key to sops

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -2,7 +2,7 @@
 {
   sops = {
     defaultSopsFile = ../../secrets/default.yaml;
-    age.sshKeyPaths = [ "/Users/iivusly/.ssh/id_ed25519" ];
+    age.sshKeyPaths = [ "/Users/iivusly/.ssh/id_ed25519" "/etc/ssh/ssh_host_ed25519_key" ];
     secrets."tailscale/nixos_server" = {};
   };
 }
