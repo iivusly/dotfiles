cd2912c7dac431cf8816ef5d817f517e2725f846
iivusly <52052910+iivusly@users.noreply.github.com>
2026-01-12 20:24:17 -0800

    feat: add my key and pass to server

---
+++ modules/server/users.nix
@@ -0,0 +1,8 @@
+{config, ...}: {
+  users.users."root" = {
+    initialHashedPassword = "$y$j9T$daAEsGbw9jxaZBF3uGhym/$2ApFrwIRRKNzETMDlTEdFT2CAQmcAiryEZ9HE62Iyg7";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh1KwHn5up7zMoXxZoYpD2W4NCafK4WSJHMESBd6cKz iivusly"
+    ];
+  };
+}
