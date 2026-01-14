31723bb962b94a5699b00a36cdb2c78a7a9e046b
iivusly <52052910+iivusly@users.noreply.github.com>
2026-01-13 21:21:26 -0800

    fix: name

--- hosts/nixos-server/disko-config.nix
+++ hosts/nixos-server/disko-config.nix
@@ -36,7 +36,7 @@
         };
       };
       external = {
-        device = "/dev/disk/by-id/usb-ST950032_5AS_WD-WX11A31Z1140-0";
+        device = "/dev/disk/by-id/usb-ST950032_5AS_WD-WX11A31Z1140-0:0";
         type = "disk";
         content = {
           type = "gpt";
