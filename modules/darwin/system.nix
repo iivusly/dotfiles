7ad2b04eec2bc61bfc332cd908c6f09df6d3a89f
iivusly <52052910+iivusly@users.noreply.github.com>
2026-03-09 22:55:06 -0700

    chore: cleanup dock

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   ...
 }:
@@ -6,12 +7,14 @@
   security.pam.services.sudo_local.touchIdAuth = true;
   system.defaults.dock.persistent-apps = [
     { app = "/System/Applications/Apps.app"; }
-    { app = "/Applications/Chromium.app"; }
+    {
+      app = "${
+        config.home-manager.users.${config.user}.programs.firefox.package
+      }/Applications/Firefox.app";
+    }
     { app = "${pkgs.telegram-desktop}/Applications/Telegram.app"; }
-    { app = "${pkgs.vesktop}/Applications/Vesktop.app"; }
-    { app = "${pkgs.element-desktop}/Applications/Element.app"; }
     { app = "/Applications/WhatsApp.app"; }
-    { app = "/Applications/Thunderbird.app"; }
+    { app = "${pkgs.thunderbird}/Applications/Thunderbird.app"; }
     { app = "${pkgs.supersonic}/Applications/Supersonic.app"; }
     { app = "${pkgs.alacritty}/Applications/Alacritty.app"; }
   ];
