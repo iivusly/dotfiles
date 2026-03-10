24b397a6746454ddcc24e6ef32b54be821656720
iivusly <52052910+iivusly@users.noreply.github.com>
2026-03-09 22:53:25 -0700

    fix: remove common sops

--- modules/common/sops.nix
+++
@@ -1,7 +0,0 @@
-{ config, ... }:
-{
-  sops = {
-    defaultSopsFile = ../../secrets/default.yaml;
-    age.sshKeyPaths = [ "/Users/iivusly/.ssh/id_ed25519" "/etc/ssh/ssh_host_ed25519_key" ];
-  };
-}
