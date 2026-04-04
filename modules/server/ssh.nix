678133bd7fd8ae4f37d40b79d2ffe330e08190c4
iivusly <52052910+iivusly@users.noreply.github.com>
2026-04-04 14:25:36 -0700

    chore: move key

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -6,4 +6,8 @@
       PasswordAuthentication = false;
     };
   };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh1KwHn5up7zMoXxZoYpD2W4NCafK4WSJHMESBd6cKz iivusly"
+  ];
 }
--- modules/server/users.nix
+++ modules/server/users.nix
@@ -2,9 +2,6 @@
 {
   users.users."root" = {
     initialHashedPassword = "$y$j9T$daAEsGbw9jxaZBF3uGhym/$2ApFrwIRRKNzETMDlTEdFT2CAQmcAiryEZ9HE62Iyg7";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEh1KwHn5up7zMoXxZoYpD2W4NCafK4WSJHMESBd6cKz iivusly"
-    ];
   };
 
   users.groups.storage = {};
