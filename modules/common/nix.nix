06b406fbda1d7a83bf19f17dd7426aa0f5870ae1
iivusly <52052910+iivusly@users.noreply.github.com>
2026-06-15 16:45:32 -0700

    feat: add cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -16,6 +16,13 @@
         "flakes"
       ];
       warn-dirty = false;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
