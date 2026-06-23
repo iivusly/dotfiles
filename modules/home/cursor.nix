7ac08e034533b9a0ee211c2425982bbc3a1be636
iivusly <52052910+iivusly@users.noreply.github.com>
2026-06-23 16:42:53 -0700

    feat: move cursor

---
+++ modules/home/cursor.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 24;
+    x11 = {
+      enable = true;
+      defaultCursor = "Adwaita";
+    };
+  };
+}
