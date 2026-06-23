31bdb408a734eeb190bb360802f2ac06188c5a1b
iivusly <52052910+iivusly@users.noreply.github.com>
2026-06-23 16:48:02 -0700

    feat: format default for host

--- hosts/nixos-msi/default.nix
+++ hosts/nixos-msi/default.nix
@@ -1,12 +1,14 @@
 {
   inputs,
   outputs,
+  globals,
   ...
-}: inputs.nixpkgs.lib.nixosSystem {
+}: inputs.nixpkgs.lib.nixosSystem rec {
   system = "x86_64-linux";
   specialArgs = {
     util = (import ../../util);
-    inherit outputs;
+    firefox-addons = inputs.firefox-addons.packages.${system};
+    inherit inputs outputs globals;
   };
 
   modules = [
@@ -16,5 +18,35 @@
     ./hardware-configuration.nix
     ../../modules/shared
     ../../modules/linux
-  ];
+     inputs.home-manager.darwinModules.home-manager
+    {
+      nixpkgs.overlays = [
+        outputs.overlays.unstable-packages
+        inputs.rust-overlay.overlays.default
+      ];
+
+      home-manager = {
+        extraSpecialArgs = specialArgs;
+        sharedModules = [
+          {
+            nixpkgs.overlays = [
+              outputs.overlays.unstable-packages
+              inputs.rust-overlay.overlays.default
+            ];
+            nixpkgs.config.allowUnfree = true;
+          }
+        ];
+      };
+
+      home-manager.users.${globals.user} = {
+        imports = [
+          inputs.nix-index-database.homeModules.nix-index
+          inputs.nixvim.homeModules.nixvim
+          inputs.stylix.homeModules.stylix
+          inputs.sops-nix.homeManagerModules.sops
+          ../../modules/home
+        ];
+      };
+    }
+ ];
 }
