6b004b577f5d21262f1a28acbb0424f0eb1f39b8
iivusly <52052910+iivusly@users.noreply.github.com>
2026-06-23 18:32:33 -0700

    fix: reference in hm

--- modules/linux/greetd.nix
+++ modules/linux/greetd.nix
@@ -4,8 +4,8 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${config.wayland.windowManager.sway.package}/bin/sway";
-        user = "iivusly";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${config.home-manager.users.${config.user}.wayland.windowManager.sway.package}/bin/sway";
+        user = config.user;
       };
     };
   };
