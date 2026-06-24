7414b29cc78f8120f0e020473a14a860a9f52534
iivusly <52052910+iivusly@users.noreply.github.com>
2026-06-24 04:15:38 +0000

    fix: update sops

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -4,6 +4,7 @@
     age.sshKeyPaths = [
       "/Users/iivusly/.ssh/id_ed25519"
       "/etc/ssh/ssh_host_ed25519_key"
+      "/home/iivusly/.ssh/id_ed25519"
     ];
     defaultSopsFile = ./secrets.yaml;
   };
