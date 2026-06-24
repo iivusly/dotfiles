422631a8bb8854504e809c511c3ac3cd16bb1bbf
iivusly <52052910+iivusly@users.noreply.github.com>
2026-06-23 18:04:12 -0700

    feat: initial hashed password for user

--- modules/linux/users.nix
+++ modules/linux/users.nix
@@ -2,6 +2,7 @@
 {
   users.users.${config.user} = {
     isNormalUser = true;
+    initialHashedPassword = "$y$j9T$daAEsGbw9jxaZBF3uGhym/$2ApFrwIRRKNzETMDlTEdFT2CAQmcAiryEZ9HE62Iyg7";
     extraGroups = [
       "wheel"
       "video"
